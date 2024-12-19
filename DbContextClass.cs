915321f8c8861b4af3898a71123032c0e13caf06
IbrahimAbdElHamid758 <ibrahimelhamid5@gmail.com>
2024-12-19 09:51:59 +0200

    Add files via upload

---
+++ DbContextClass.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RedisApp.Models;
+
+namespace RedisApp.Data
+{
+    public class DbContextClass :DbContext
+    {
+        public DbContextClass(DbContextOptions<DbContextClass> options):base(options)
+        {}
+        public DbSet<Product> Products { get; set; }
+    }
+}
